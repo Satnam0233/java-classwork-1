9f63cdda19576939dfd5c53b857ec7ffb5c41cb3
gurisatnam <gurisatnam@DESKTOP-FRNR52U>
2019-06-11 14:41:14 -0700

    first commit

---
+++ java classwork 1/src/practiceday1/add.java
@@ -0,0 +1,13 @@
+package practiceday1;
+
+public class add {
+
+	public static void main(String[] args) {
+			add(10,20);
+	}
+	public static add(int a,int b) {
+		int c= a+b;
+		System.out.println(c);
+	}
+
+}
